8af89e6ba98dcbec133fd30427e3c23087b04905
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-10-27 18:02:04 -0500

    se configuro sass

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebapckPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports={
+    entry:'./src/index.js',//archivo principal de entrada
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'boundle.js',        
+    },
+    mode: 'development',
+    resolve: {
+        extensions:['.js', '.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+
+                    
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebapckPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    
+}
