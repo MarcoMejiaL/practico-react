641e832322a024befd2ca93abcaa9f5e610b16f4
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-11-09 19:01:11 -0600

    estados de react

---
+++ estados/ejemplo1.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+/**
+ * @name StateFull
+ * @description trabaja con un estado, creando una constante sobre el return 
+ * se genera una constante para trabajar con los hoks se le asigna [name, setName]
+ * se le da un = useState('esto es el estado') <= eso usa el estado y lo pasa Nota: se importa arriba
+ * 
+ * @returns nose
+ */
+
+const Button =()=>{
+    const [name,SetNmae] =useState('hola Mundo ');//aqui va el estado
+    return(
+        <div>
+            <h1>{name}</h1>
+        </div>
+    );
+
+}
+
+/**
+ * @name stateless
+ * 
+ * @description Los componentes stateless servirán para pasar un estilo visual o props, pero no tendrá otra función más que esa.
+
+Este sería un componente sin estado, stateless.
+ * @returns 
+ */
+
+const button =()=> <Button/>
+
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hola Mundo</h1>
+            </div>
+        )
+    }
+}
+
+/**
+ * @name Hook
+ * 
+ * @description 
+ */
+
+function ComponentesWrapper(WrrapperComponente){
+
+    clas
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,55 @@
 const path = require('path');
-const HtmlWebapckPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
-module.exports={
-    entry:'./src/index.js',//archivo principal de entrada
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'boundle.js',   
-        publicPath:'/',     
-    },
-    mode: 'development',
-    resolve: {
-        extensions:['.js', '.jsx']
-    },
-    module:{
-        rules:[
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use:{
-                    loader:'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    }
-                ]
-            },
-            {
-                test: /\.(css|scss)$/,
-                use:[
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader",
-
-                    
-                ]
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebapckPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        }),
-    ],
-    devServer:{
-        historyApiFallback: true,
-    }
-    
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+	],
+	devServer: {
+		historyApiFallback: true,
+	}
 }
